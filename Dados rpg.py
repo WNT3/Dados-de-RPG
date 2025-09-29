0bb13a2b4b63d320fd8a113c11f54f59d2858918
WNT3 <110870830+WNT3@users.noreply.github.com>
2025-09-28 22:34:09 -0300

    Add files via upload

---
+++ Dados rpg.py
@@ -0,0 +1,19 @@
+dadodvinte = int(input('Insira o valor do dado D20: '))
+print ('O valor do dado D20 é: ', dadodvinte)
+if dadodvinte == 20:
+    print ('Você acertou um crítico!')
+else:
+    print ('Você não acertou o crítico')
+    exit ()
+if dadodvinte == 20:
+    dadodcem = int(input('Insira o valor do dado D100: '))
+    print ('O valor do dado D100 é: ', dadodcem,)
+    if 100 >= dadodcem > 95:
+        print ('Você passou!')
+        exit()
+    
+if 94 > dadodcem > 90:
+    print ('Chute um valor para um dado D4 e jogue o dado, caso caia o número chutado, você acertará o FF, caso o contrário, o progresso será salvo e o próximo crítico voltará desse estado.')
+if 94 > dadodcem > 90:
+    dadodquatro = int(input('Digite o valor de D4: '))
+print ('O valor do dado D4 é: ', dadodquatro)
